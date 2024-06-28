aa98f89848d35bc5b14ee094dd67ccdd77afa28f
Helen <lenagreidon@mail.ru>
2024-06-28 14:04:18 +0300

    added router component in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,35 @@
+import { useEffect } from "react";
+import ScrollToTop from "@components/scrollToTop/ScrollToTop";
+import RouterComponent from "@components/router/RouterComponent";
+import { ToastContainer, Zoom } from "react-toastify";
+import { useAppSelector } from "@store/hook";
+import * as tutorSelectors from "@store/selectors";
+import { translate } from "@i18n";
+
 const App = () => {
-  return <div>Hello!</div>;
+  const locale = useAppSelector(tutorSelectors.localeSelect);
+  const { i18n } = translate();
+
+  useEffect(() => {
+    i18n.changeLanguage(locale);
+    // eslint-disable-next-line
+  }, [locale]);
+
+  return (
+    <>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        theme="light"
+        className={"notificationToast"}
+        toastClassName={"toastBody"}
+        progressClassName={"toastProgress"}
+        transition={Zoom}
+      />
+      <ScrollToTop />
+      <RouterComponent />
+    </>
+  );
 };
 
 export default App;
