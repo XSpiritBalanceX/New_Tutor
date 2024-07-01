c111ad3d6e9e73d8777eafae7646ddf78fdf54ed
Helen <lenagreidon@mail.ru>
2024-07-01 12:08:43 +0300

    added protected route

--- src/components/router/ProtectedRouter.tsx
+++ src/components/router/ProtectedRouter.tsx
@@ -2,17 +2,18 @@ import { Navigate } from "react-router-dom";
 import { useAppSelector } from "@store/hook";
 import * as tutorSelectors from "@store/selectors";
 
-interface IProtectedRouterForLoggedProps {
+interface IProtectedRouterProps {
   children: JSX.Element;
 }
 
-const ProtectedRouterForLogged = ({ children }: IProtectedRouterForLoggedProps) => {
+const ProtectedRouter = ({ children }: IProtectedRouterProps) => {
+  const token = localStorage.getItem("tutor_access_token");
   const isLogin = useAppSelector(tutorSelectors.isLoginSelect);
 
-  if (!isLogin) {
+  if (!token || !isLogin) {
     return <Navigate to={"/"} replace />;
   }
   return children;
 };
 
-export default ProtectedRouterForLogged;
+export default ProtectedRouter;
--- src/components/router/RouterComponent.tsx
+++ src/components/router/RouterComponent.tsx
@@ -1,21 +1,22 @@
 import { Route, Routes, Navigate } from "react-router-dom";
 import ProtectedRouterForLogged from "./ProtectedRouterForLogged";
+import ProtectedRouter from "./ProtectedRouter";
 import AuthPage from "@pages/auth/AuthPage";
 
 const RouterComponent = () => {
-  const unprotectedRoutes = [
+  const unauthRoutes = [
     { path: "/login", element: <AuthPage /> },
     { path: "/registration", element: <AuthPage /> },
   ];
 
-  const protectedRoutes = [{ path: "", element: <div></div> }];
+  const authRoutes = [{ path: "", element: <div></div> }];
   return (
     <Routes>
-      {unprotectedRoutes.map((el, ind) => (
-        <Route key={ind} path={el.path} element={el.element} />
-      ))}
-      {protectedRoutes.map((el, ind) => (
+      {unauthRoutes.map((el, ind) => (
         <Route key={ind} path={el.path} element={<ProtectedRouterForLogged>{el.element}</ProtectedRouterForLogged>} />
+      ))}
+      {authRoutes.map((el, ind) => (
+        <Route key={ind} path={el.path} element={<ProtectedRouter>{el.element}</ProtectedRouter>} />
       ))}
       <Route path="*" element={<Navigate to={"/"} />} />
     </Routes>
