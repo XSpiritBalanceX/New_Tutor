913c9a301e4bfcfb9adefbcc22c2690d893ab0fc
Helen <lenagreidon@mail.ru>
2024-08-01 14:25:53 +0300

    added api call for lessons page

--- src/pages/allLessons/AllLessonsPage.tsx
+++ src/pages/allLessons/AllLessonsPage.tsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
-import { Container, Box } from "@mui/material";
+import { Container, Box, Pagination } from "@mui/material";
 import { translate } from "@i18n";
 import KeyboardArrowRightOutlinedIcon from "@mui/icons-material/KeyboardArrowRightOutlined";
 import { useParams, useNavigate } from "react-router-dom";
 import { USER_TYPE } from "@axiosApi/axiosAPI";
 import CustomError from "@components/error/CustomError";
 import Loader from "@components/loader/Loader";
+import { useGetLessonsQuery } from "@store/requestApi/lessonsApi";
+import calendar from "@assets/calendar.svg";
 import "./AllLessonsPage.scss";
 
 const AllLessonsPage = () => {
@@ -18,14 +20,48 @@ const AllLessonsPage = () => {
 
   const [itemPerPage] = useState(5);
 
-  return (
+  const { data, error, isFetching } = useGetLessonsQuery({ countLessons: itemPerPage, currentPage: Number(page) });
+
+  console.log(data);
+
+  const handleChangePage = (_: React.ChangeEvent<unknown>, value: number) => {
+    navigate(`/lessons/${value}`);
+  };
+
+  return error ? (
+    <CustomError />
+  ) : (
     <Container className="allLessonsPageContainer">
+      {isFetching && <Loader />}
       <Box className="locationAllLessons">
         <p className="myLessons">{t("myLessons")}</p>
         <KeyboardArrowRightOutlinedIcon className="arrowIcon" />
         <p className="allLesson">{t("allLessons")}</p>
       </Box>
       <p className="titleAllLessons">{t("allLessons")}</p>
+      {data && (
+        <>
+          {data.items.length === 0 && (
+            <Box className="emptyDataBox">
+              <p>{t(isStudent ? "noOneLessons" : "noOneLessonsTeacher")}</p>
+              <img src={calendar} alt="calendar" />
+            </Box>
+          )}
+          {data.items.length !== 0 && (
+            <Box className="lessonsContainer">
+              <p className="titleLessons">{t("upcomingLessons")}</p>
+            </Box>
+          )}
+          <Box className="paginationBox">
+            <Pagination
+              count={data.count}
+              page={Number(page)}
+              onChange={handleChangePage}
+              className="searchPagination"
+            />
+          </Box>
+        </>
+      )}
     </Container>
   );
 };
